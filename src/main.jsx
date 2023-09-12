3c7be5c803d7304635d799d7e1a46f8b496b1bae
Bahadir UNGOR <bahadirungor@gmail.com>
2023-09-12 20:04:02 +0300

    boilerplate finished

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './assets/styles/global.css'
+import Header from './components/header/Header'
+import Footer from './components/footer/footer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Header/>
+    <main></main>
+    <Footer/>
   </React.StrictMode>,
 )
