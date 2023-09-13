f32a2db0d47ff57e475703b6fd531565b4fb0bed
Bahadir UNGOR <bahadirungor@gmail.com>
2023-09-13 09:05:15 +0300

    ui trial

--- src/components/mainContent/mainContent.jsx
+++ src/components/mainContent/mainContent.jsx
@@ -3,6 +3,7 @@ import './mainContent.css'
 export default function MainContent(){
 
     return (
+        
         <main>
             <div className='settings'>deeme</div>
             <div className='preview'>deneme</div>
